37d6ada675f0e3360fe8504de19a1de98b15d34b
r3tx <nossrednav@gmail.com>
2016-08-16 12:25:28 -0500

    primera parte

---
+++ app/src/main/java/lab1ui/gr7/compumovil/udea/edu/co/lab1ui/MainActivity.java
@@ -0,0 +1,13 @@
+package lab1ui.gr7.compumovil.udea.edu.co.lab1ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
