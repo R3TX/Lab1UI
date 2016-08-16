e5aa5d02b56a659dfddcef40f428ffc38d00201e
r3tx <nossrednav@gmail.com>
2016-08-16 13:06:49 -0500

    spiner

--- app/src/main/java/lab1ui/gr7/compumovil/udea/edu/co/lab1ui/MainActivity.java
+++ app/src/main/java/lab1ui/gr7/compumovil/udea/edu/co/lab1ui/MainActivity.java
@@ -2,6 +2,8 @@ package lab1ui.gr7.compumovil.udea.edu.co.lab1ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinnerPaises = (Spinner) findViewById(R.id.sPaises);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.paises, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinnerPaises.setAdapter(adapter);
+        Spinner spinnerHobbies = (Spinner) findViewById(R.id.sHobbies);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.hobbies, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinnerHobbies.setAdapter(adapter2);
     }
 }
